f4387583f40ad10dd47a91e40ce0fc24cb0df0fe
Ieva Aleksandravica <ieva.aleksandravica@gmail.com>
2021-08-18 15:11:58 +0200

    added order items to product order page

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,5 +2,9 @@ class ProductsController < ApplicationController
   def show
     @product = Product.find(params[:id])
     @order_item = OrderItem.new
+    @user = current_user
+    @user_orders = Order.where(user: current_user)
+    @order = @user_orders.last
+    @all_order_items = OrderItem.where(order: @order)
   end
 end
