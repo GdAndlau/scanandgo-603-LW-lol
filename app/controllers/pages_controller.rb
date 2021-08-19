2722801ee4f9a4ae3c7679738e152fbb8b42ac1d
Ieva Aleksandravica <ieva.aleksandravica@gmail.com>
2021-08-19 21:21:44 +0200

    home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,9 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    if current_user
+    redirect_to stores_path
+    else
+    end
   end
 end
