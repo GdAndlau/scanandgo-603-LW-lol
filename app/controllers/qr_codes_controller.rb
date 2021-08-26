ab8b8706096d85aae5d868d17410f429c4d47c74
Ghislain d'Andlau <ghislain.dandlau@gmail.com>
2021-08-26 20:36:19 +0200

    do not merge

---
+++ app/controllers/qr_codes_controller.rb
@@ -0,0 +1,27 @@
+class QrCodesController < ApplicationController
+
+  def index
+    @qr_codes = QrCode.order(created_at: :desc).first(20)
+  end
+
+  def new
+  end
+
+  def create
+    qr_code = QrCode.create(content: @qr_data)
+
+    redirect_to qr_code_path(qr_code)
+  end
+
+  def show
+    @qr_code = QrCode.find(params[:id])
+  end
+
+  private
+
+  def set_qr_data
+    qr_code_params = JSON.parse(params[:qr_code_json_data]).with_indifferent_access
+
+    @qr_data = qr_code_params[:qr_data]
+  end
+end
