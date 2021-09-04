64c16ec0e6a95991ed94478f9b96a8b4c9af884d
Wolfgang Ernst <mail@wolfgangernst.com>
2021-09-04 12:07:19 +0200

    Payment for one item working

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -6,25 +6,24 @@ class OrderItemsController < ApplicationController
     @order = @user_orders.last
     @order_item.product = @product_id
     @order_item.order = @order
-
+    @order_item.save!
     product = Product.find(params[:product_id])
       order = @user_orders.last
-      order  = Order.update(@order_item.product = product, price_cents = product.price, state ='pending', user_id = current_user.id)
+      order  = Order.update(completed: "pending", price_cents: @order_item.product.price_cents, state: nil, product_sku: nil, checkout_session_id: nil)
       session = Stripe::Checkout::Session.create(
         payment_method_types: ['card'],
         line_items: [{
-          name: product.name,
-          images: [product.photo_url],
+          name: @order_item.product.title,
+          # images: [@order_item.product.image_url],
           amount: product.price_cents,
           currency: 'eur',
-          quantity: @all_order_items.count #Needs to be fixed
+          quantity: 1 #Needs to be fixed
         }],
-        success_url: order_url(order),
-        cancel_url: order_url(order)
+        success_url: qrcode_url(order),
+        cancel_url: new_qr_code_url(order)
       )
 
-      order.update(checkout_session_id: session.id)
-      redirect_to new_orders_payment_path(order)
+      @order.update(checkout_session_id: session.id)
 
     if @order_item.save!
       redirect_to new_qr_code_path
--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,13 +1,14 @@
 class PaymentsController < ApplicationController
   def new
     @user = current_user
-    @order = Order.where(user: current_user)
-    @last_order = @order.last
-    @all_order_items = OrderItem.where(order: @last_order).order('created_at DESC')
+    @user_orders = Order.where(user: current_user)
+    @order = @user_orders.last
+    @all_order_items = OrderItem.where(order: @order).order('created_at DESC')
     @total_price = 0
     @all_order_items.each do |item|
     @total_price = @total_price + item.product.price
-    raise
+
+
     end
 
   end
